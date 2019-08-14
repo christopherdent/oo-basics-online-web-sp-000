7251b5a342ee2b90f798496904ba2aec374f4793
Christopher Dent <christopherdent01@gmail.com>
2019-08-14 01:15:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,15 +5,16 @@ class Shoe
   
   attr_accessor :color, :size, :material, :condition
   
+  attr_reader "brand"
+  
   def initialize(brand)
     @brand = brand 
   end 
   
-  def brand
-    @brand 
-  end 
+ 
   
-  
+
+ 
   
   
   
